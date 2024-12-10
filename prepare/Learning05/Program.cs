f339033412c6a72d34169380f63558c14831da04
alberrtoghj <niesidestiny@gmail.com>
2024-12-10 15:52:56 +0100

    update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,9 +3,9 @@ using System;
 class Program
 {
     static void Main(string[] args)
-   {
-        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
-        Console.WriteLine(mathAssignment.GetSummary());
-        Console.WriteLine(mathAssignment.GetHomeWork());
+    {
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }

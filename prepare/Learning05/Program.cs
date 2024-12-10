4221151a665bd0c6200daed5d5b6721401bdac10
alberrtoghj <niesidestiny@gmail.com>
2024-12-10 14:50:27 +0100

    update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,8 +3,9 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
-        Console.WriteLine(assignment.GetSummary());
+   {
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeWork());
     }
 }
